3f6e3f48b9870568457cd7697ee6b52fbe9233e0
Zhi Zhang <zhangzhi@rcitech.cn>
2018-02-08 19:04:28 +0800

    sumbit real DRC Token contract

--- contracts/DRCTestFlattenedToken.sol
+++ contracts/DRCTestFlattenedToken.sol
@@ -338,9 +338,9 @@ contract PausableToken is StandardToken, Pausable {
 
 contract DRCTestToken is BurnableToken, MintableToken, PausableToken {    
 
-    string public name = 'DRC Test Token 1';
+    string public name = 'DRC Test Token';
 
-    string public symbol = 'DRC1';
+    string public symbol = 'DRCT';
 
     uint8 public decimals = 18;
 
@@ -348,34 +348,26 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
 
     // add map for recording the accounts that will not be allowed to transfer tokens
     mapping (address => bool) public frozenAccount;
-    event FrozenFunds(address target, bool frozen);
+    event FrozenFunds(address _target, bool _frozen);
 
     /**
-
      * contruct the token by total amount 
-
      *
-
-     * there are 3 phases for releasing the tokens, initial balance is set. 
-
+     * initial balance is set. 
      */
-
     function DRCTestToken() public {
-
         totalSupply = INITIAL_SUPPLY;
-
         balances[msg.sender] = totalSupply;
-
     }
-
+    
     /**
      * freeze the account's balance 
      *
      * by default all the accounts will not be frozen until set freeze value as true. 
      */
-    function freezeAccount(address target, bool freeze) onlyOwner public {
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
+    function freezeAccount(address _target, bool _freeze) onlyOwner public {
+        frozenAccount[_target] = _freeze;
+        FrozenFunds(_target, _freeze);
     }
 
   /**
@@ -383,29 +375,31 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
    * @param _to The address to transfer to.
    * @param _value The amount to be transferred.
    */
-  function transfer(address _to, uint256 _value) public returns (bool) {
+  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
     require(!frozenAccount[msg.sender]);
     return super.transfer(_to, _value);
   }
-
+  
   /**
    * @dev Transfer tokens from one address to another with checking the frozen status
    * @param _from address The address which you want to send tokens from
    * @param _to address The address which you want to transfer to
    * @param _value uint256 the amount of tokens to be transferred
    */
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
     require(!frozenAccount[_from]);
     return super.transferFrom(_from, _to, _value);
   }
- 
+
   /**
    * @dev transfer token for a specified address with froze status checking
    * @param _toMulti The addresses to transfer to.
    * @param _values The array of the amount to be transferred.
    */
-  function transferMultiAddress(address[] _toMulti, uint256[] _values) public returns (bool) {
+  function transferMultiAddress(address[] _toMulti, uint256[] _values) public whenNotPaused returns (bool) {
     require(!frozenAccount[msg.sender]);
+    assert(_toMulti.length == _values.length);
+
     uint256 i = 0;
     while ( i < _toMulti.length) {
         require(_toMulti[i] != address(0));
@@ -425,11 +419,12 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
   /**
    * @dev Transfer tokens from one address to another with checking the frozen status
    * @param _from address The address which you want to send tokens from
-   * @param _toMulti address[] The addresses which you want to transfer to
-   * @param _values uint256[] the array of the amounts of tokens to be transferred
+   * @param _toMulti address[] The addresses which you want to transfer to in boundle
+   * @param _values uint256[] the array of amount of tokens to be transferred
    */
-  function transferMultiAddressFrom(address _from, address[] _toMulti, uint256[] _values) public returns (bool) {
+  function transferMultiAddressFrom(address _from, address[] _toMulti, uint256[] _values) public whenNotPaused returns (bool) {
     require(!frozenAccount[_from]);
+    assert(_toMulti.length == _values.length);
     
     uint256 i = 0;
     while ( i < _toMulti.length) {
@@ -447,42 +442,52 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
     }
 
     return true;
-  } 
+  }
+  
     /**
-
-     * Destroy tokens from other account
-
-     *
-
-     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-
-     *
-
-     * @param _from the address of the sender
-
-     * @param _value the amount of money to burn
-
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
      */
-
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-
-        require(balances[_from] >= _value);                // Check if the targeted balance is enough
-
-        require(_value <= allowed[_from][msg.sender]);    // Check allowance
-
-        balances[_from] = balances[_from].sub(_value);                         // Subtract from the targeted balance
-
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);             // Subtract from the sender's allowance
-
-        totalSupply = totalSupply.sub(_value);
-
-        Burn(_from, _value);
-
-        return true;
-
+    function burn(uint256 _value) whenNotPaused public {
+        super.burn(_value);
     }
 
-    
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public whenNotPaused returns (bool success) {
+        require(balances[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowed[_from][msg.sender]);    // Check allowance
+        balances[_from] = balances[_from].sub(_value);                         // Subtract from the targeted balance
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);             // Subtract from the sender's allowance
+        totalSupply = totalSupply.sub(_value);
+        Burn(_from, _value);
+        return true;
+    }
+
+  /**
+   * @dev Function to mint tokens
+   * @param _to The address that will receive the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address _to, uint256 _amount) onlyOwner canMint whenNotPaused public returns (bool) {
+      return super.mint(_to, _amount);
+  }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting() onlyOwner canMint whenNotPaused public returns (bool) {
+      return super.finishMinting();
+  }
+
 
     /**
 
@@ -502,7 +507,7 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
 
      */
 
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public whenNotPaused returns (bool success) {
 
         tokenRecipient spender = tokenRecipient(_spender);
 
--- contracts/DRCTestToken.sol
+++ contracts/DRCTestToken.sol
@@ -15,14 +15,14 @@ interface tokenRecipient {
 }
 
 contract DRCTestToken is BurnableToken, MintableToken, PausableToken {    
-    string public name = 'DRC Test Token 1';
-    string public symbol = 'DRC1';
+    string public name = 'DRC Test Token';
+    string public symbol = 'DRCT';
     uint8 public decimals = 18;
     uint public INITIAL_SUPPLY = 1000000000000000000000000000;
 
     // add map for recording the accounts that will not be allowed to transfer tokens
     mapping (address => bool) public frozenAccount;
-    event FrozenFunds(address target, bool frozen);
+    event FrozenFunds(address _target, bool _frozen);
 
     /**
      * contruct the token by total amount 
@@ -39,9 +39,9 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
      *
      * by default all the accounts will not be frozen until set freeze value as true. 
      */
-    function freezeAccount(address target, bool freeze) onlyOwner public {
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
+    function freezeAccount(address _target, bool _freeze) onlyOwner public {
+        frozenAccount[_target] = _freeze;
+        FrozenFunds(_target, _freeze);
     }
 
   /**
@@ -49,7 +49,7 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
    * @param _to The address to transfer to.
    * @param _value The amount to be transferred.
    */
-  function transfer(address _to, uint256 _value) public returns (bool) {
+  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
     require(!frozenAccount[msg.sender]);
     return super.transfer(_to, _value);
   }
@@ -60,7 +60,7 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
    * @param _to address The address which you want to transfer to
    * @param _value uint256 the amount of tokens to be transferred
    */
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
     require(!frozenAccount[_from]);
     return super.transferFrom(_from, _to, _value);
   }
@@ -70,8 +70,10 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
    * @param _toMulti The addresses to transfer to.
    * @param _values The array of the amount to be transferred.
    */
-  function transferMultiAddress(address[] _toMulti, uint256[] _values) public returns (bool) {
+  function transferMultiAddress(address[] _toMulti, uint256[] _values) public whenNotPaused returns (bool) {
     require(!frozenAccount[msg.sender]);
+    assert(_toMulti.length == _values.length);
+
     uint256 i = 0;
     while ( i < _toMulti.length) {
         require(_toMulti[i] != address(0));
@@ -81,6 +83,8 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
         balances[msg.sender] = balances[msg.sender].sub(_values[i]);
         balances[_toMulti[i]] = balances[_toMulti[i]].add(_values[i]);
         Transfer(msg.sender, _toMulti[i], _values[i]);
+
+        i = i.add(1);
     }
 
     return true;
@@ -92,8 +96,9 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
    * @param _toMulti address[] The addresses which you want to transfer to in boundle
    * @param _values uint256[] the array of amount of tokens to be transferred
    */
-  function transferMultiAddressFrom(address _from, address[] _toMulti, uint256[] _values) public returns (bool) {
+  function transferMultiAddressFrom(address _from, address[] _toMulti, uint256[] _values) public whenNotPaused returns (bool) {
     require(!frozenAccount[_from]);
+    assert(_toMulti.length == _values.length);
     
     uint256 i = 0;
     while ( i < _toMulti.length) {
@@ -106,11 +111,21 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
         balances[_toMulti[i]] = balances[_toMulti[i]].add(_values[i]);
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_values[i]);
         Transfer(_from, _toMulti[i], _values[i]);
+
+        i = i.add(1);
     }
 
     return true;
   }
   
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) whenNotPaused public {
+        super.burn(_value);
+    }
+
     /**
      * Destroy tokens from other account
      *
@@ -119,7 +134,7 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
      * @param _from the address of the sender
      * @param _value the amount of money to burn
      */
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+    function burnFrom(address _from, uint256 _value) public whenNotPaused returns (bool success) {
         require(balances[_from] >= _value);                // Check if the targeted balance is enough
         require(_value <= allowed[_from][msg.sender]);    // Check allowance
         balances[_from] = balances[_from].sub(_value);                         // Subtract from the targeted balance
@@ -128,6 +143,24 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
         Burn(_from, _value);
         return true;
     }
+
+  /**
+   * @dev Function to mint tokens
+   * @param _to The address that will receive the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address _to, uint256 _amount) onlyOwner canMint whenNotPaused public returns (bool) {
+      return super.mint(_to, _amount);
+  }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting() onlyOwner canMint whenNotPaused public returns (bool) {
+      return super.finishMinting();
+  }
     
     /**
      * Set allowance for other address and notify
@@ -138,7 +171,7 @@ contract DRCTestToken is BurnableToken, MintableToken, PausableToken {
      * @param _value the max amount they can spend
      * @param _extraData some extra information to send to the approved contract
      */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public whenNotPaused returns (bool success) {
         tokenRecipient spender = tokenRecipient(_spender);
         if (approve(_spender, _value)) {
             spender.receiveApproval(msg.sender, _value, this, _extraData);
