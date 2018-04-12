d06856badd926832ea2c5041e7e3bc412dd7afc7
Zhi Zhang <zhangzhi@rcitech.cn>
2018-04-13 06:48:09 +0800

    add usefull script

---
+++ contractUtility/test_time.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var timeseconds = new Date(2018,2,12,9,0,0).getTime() / 1000 + 86400 * 365;
+console.log(timeseconds);
+console.log(86400 * 365 * 2);
+//var nowtime = new Date().getTime() / 1000;
+//console.log(nowtime);
+//var newtime = new Date(1520867875 * 1000);
+//console.log(newtime);
+//var endtime = new Date(2018, 2, 12, 14, 5, 17).getTime() / 1000;
+//var mul = (1520867875 - endtime) / 600;
+//console.log(mul);
+
