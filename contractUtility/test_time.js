a98185db4447d848be43c5933f042010160be9de
Zhi Zhang <zhangzhi@rcitech.cn>
2018-04-24 22:23:13 +0800

    modify some value

--- contractUtility/test_time.js
+++ contractUtility/test_time.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-var timeseconds = new Date(2018,2,12,9,0,0).getTime() / 1000 + 86400 * 365;
+var timeseconds = new Date(2018,3,23,9,0,0).getTime() / 1000 + 86400 * 182;
 console.log(timeseconds);
 console.log(86400 * 365 * 2);
 //var nowtime = new Date().getTime() / 1000;
