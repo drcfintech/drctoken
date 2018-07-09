ccec9bf4ae1fb209b52d5ec68d5d70bb12fc1f12
张治 <zzdxzhangzhi@msn.com>
2018-07-09 09:53:02 +0800

    add limit for once mint tokens

--- contracts/DRCTOwner.sol
+++ contracts/DRCTOwner.sol
@@ -8,6 +8,7 @@ interface itoken {
     function freezeAccount(address _target, bool _freeze) external;
     function freezeAccountPartialy(address _target, uint256 _value) external;
     function balanceOf(address _owner) external view returns (uint256 balance);
+    // function totalSupply() external view returns (uint256);
     // function transferOwnership(address newOwner) external;
     function allowance(address _owner, address _spender) external view returns (uint256);
     function initialCongress(address _congress) external;
@@ -17,11 +18,41 @@ interface itoken {
     function unpause() external;
 }
 
+// interface iParams {
+//     function reMintCap() external view returns (uint256);
+//     function onceMintRate() external view returns (uint256);
+// }
+
 contract DRCTOwner is MultiOwnerContract {
     string public constant AUTH_INITCONGRESS = "initCongress";
     string public constant AUTH_CANMINT = "canMint";
+    string public constant AUTH_SETMINTAMOUNT = "setMintAmount";
 
-    bool congressInit = true;
+    bool congressInit = false;
+    // bool paramsInit = false;
+    // iParams public params;
+    uint256 onceMintAmount;
+
+
+    // function initParams(address _params) onlyOwner public {
+    //     require(!paramsInit);
+    //     require(_params != address(0));
+
+    //     params = _params;
+    //     paramsInit = false;
+    // }
+
+    /**
+     * @dev Function to set mint token amount
+     * @param _value The mint value.
+     */
+    function setOnceMintAmount(uint256 _value) onlyMultiOwners public {
+        require(hasAuth(AUTH_SETMINTAMOUNT));
+        require(_value > 0);
+        onceMintAmount = _value;
+
+        clearAuth(AUTH_SETMINTAMOUNT);
+    }
 
     /**
      * @dev change the owner of the contract from this contract address to another one. 
@@ -30,25 +61,25 @@ contract DRCTOwner is MultiOwnerContract {
      */
     function initCongress(address _congress) onlyMultiOwners public {
         require(hasAuth(AUTH_INITCONGRESS));        
-        require(congressInit);
+        require(!congressInit);
 
         itoken tk = itoken(address(ownedContract));
         tk.initialCongress(_congress);
 
         clearAuth(AUTH_INITCONGRESS);
+        congressInit = true;
     }
 
     /**
      * @dev Function to mint tokens
      * @param _to The address that will receive the minted tokens.
-     * @param _amount The amount of tokens to mint.
      * @return A boolean that indicates if the operation was successful.
      */
-    function mint(address _to, uint256 _amount) onlyMultiOwners public returns (bool) {
+    function mint(address _to) onlyMultiOwners public returns (bool) {
         require(hasAuth(AUTH_CANMINT)); 
 
         itoken tk = itoken(address(ownedContract));
-        bool res = tk.mint(_to, _amount);
+        bool res = tk.mint(_to, onceMintAmount);
 
         clearAuth(AUTH_CANMINT);
         return res;
---
+++ contracts/DRCTParams.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.23;
+
+
+import 'zeppelin-solidity/contracts/ownership/Claimable.sol';
+import './Autonomy.sol';
+
+
+/**
+ * contract that define the wallet management parameters on DRC platform
+ * only owner could initialize the parameters, but the congress contract 
+ * could set the parameters in the future
+ */
+contract DRCWalletMgrParams is Claimable, Autonomy {
+    uint256 public reMintCap; // max value of reMint up limit
+    uint256 public onceMintRate; // Max value of once minting
+
+
+    function initialReMintCap(uint256 _value) onlyOwner public {
+        require(!init);
+
+        reMintCap = _value;
+    }
+
+    function initialOnceMintAmount(uint256 _value) onlyOwner public {
+        require(!init);
+
+        onceMintRate = _value;
+    }
+
+    function setReMintCap(uint256 _value) onlyCongress public {
+        reMintCap = _value;
+    }   
+
+    function setOnceMintAmount(uint256 _value) onlyCongress public {
+        onceMintRate = _value;
+    }
+}
