65c2cb4c688f0b59753c22f2ea14c2fb6da42988
Zhi Zhang <zhangzhi@rcitech.cn>
2018-07-20 21:53:40 +0800

    new flydrop contracts

--- contracts/FlyDropTokenMgr.sol
+++ contracts/FlyDropTokenMgr.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/ownership/DelayedClaimable.sol";
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-import './FlyDropToken.sol';
+import './SimpleFlyDropToken.sol';
 
 
-interface itoken {    
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) external returns (bool);
-}
+// interface itoken {    
+//     function approveAndCall(address _spender, uint256 _value, bytes _extraData) external returns (bool);
+// }
 
 
 /**
@@ -18,7 +18,7 @@ contract FlyDropTokenMgr is DelayedClaimable {
     using SafeMath for uint256;
 
     address[] dropTokenAddrs;
-    FlyDropToken currentDropTokenContract;
+    SimpleFlyDropToken currentDropTokenContract;
     // mapping(address => mapping (address => uint256)) budgets;
 
     /**
@@ -28,13 +28,11 @@ contract FlyDropTokenMgr is DelayedClaimable {
      * @param _from address The address which you want to send tokens from
      * @param _value uint256 the amounts of tokens to be sent
      * @param _token address the ERC20 token address
-     * @param _extraData bytes the extra data for the record
      */
     function prepare(uint256 _rand, 
                      address _from, 
                      address _token, 
-                     uint256 _value,
-                     bytes _extraData) onlyOwner public returns (bool) {
+                     uint256 _value) onlyOwner public returns (bool) {
         require(_token != address(0));
         require(_from != address(0));
         require(_rand > 0);
@@ -44,16 +42,17 @@ contract FlyDropTokenMgr is DelayedClaimable {
         }
 
         if (_rand > dropTokenAddrs.length) {
-            FlyDropToken dropTokenContract = new FlyDropToken();
+            SimpleFlyDropToken dropTokenContract = new SimpleFlyDropToken();
             dropTokenAddrs.push(address(dropTokenContract));
             currentDropTokenContract = dropTokenContract;
         } else {
-            currentDropTokenContract = FlyDropToken(dropTokenAddrs[_rand.sub(1)]);
+            currentDropTokenContract = SimpleFlyDropToken(dropTokenAddrs[_rand.sub(1)]);
         }
 
-        ERC20(_token).transferFrom(_from, this, _value);
+        currentDropTokenContract.setToken(_token);
+        return ERC20(_token).transferFrom(_from, currentDropTokenContract, _value);
         // budgets[_token][_from] = budgets[_token][_from].sub(_value);
-        return itoken(_token).approveAndCall(currentDropTokenContract, _value, _extraData);
+        // return itoken(_token).approveAndCall(currentDropTokenContract, _value, _extraData);
         // return true;
     } 
 
