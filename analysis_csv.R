c5607ae2b1a0b665ce3e78761b9d9b8285c9843a
Zhi Zhang <zhangzhi@rcitech.cn>
2019-03-23 01:15:33 +0800

    change R file for releasing token

--- analysis_csv.R
+++ analysis_csv.R
@@ -1,12 +1,12 @@
 options(scipen = 10)
-voiceURL = "C:\\Users\\rcit-001\\Documents\\工作文档\\DRC\\worktemp\\第六批病毒糖果发送（3）.csv"
+voiceURL = "C:\\Users\\rcit-001\\Documents\\工作文档\\DRC\\worktemp\\第八批病毒糖果发送（37）.csv"
 input.df = read.csv(voiceURL, header = FALSE, colClasses = 'character')
 input.df[,2] = as.numeric(input.df[,2]) * 1e18
 input.df
 
 
 
-filename = "C:\\Users\\rcit-001\\Documents\\工作文档\\DRC\\worktemp\\addresses0214-1.txt"
+filename = "C:\\Users\\rcit-001\\Documents\\工作文档\\DRC\\worktemp\\addresses0227-1-1.txt"
 cat(input.df[,1][1:200], file = filename, sep = ",")
 cat("\n", file = filename, append = TRUE)
 cat(input.df[,2][1:200], file = filename, sep = ",", append = TRUE)
@@ -16,7 +16,7 @@ cat(input.df[,1][1601:1700], file = filename, sep = ",")
 cat("\n", file = filename, append = TRUE)
 cat(input.df[,2][1601:1700], file = filename, sep = ",", append = TRUE)
 
-filename = "C:\\Users\\rcit-001\\Documents\\工作文档\\DRC\\worktemp\\addresses0223-1-1.txt"
+filename = "C:\\Users\\rcit-001\\Documents\\工作文档\\DRC\\worktemp\\addresses0312-1-1.txt"
 len = length(input.df[,2])
 len
 cat(input.df[,1][1:len], file = filename, sep = ",")
--- analysis_csv_1.R
+++ analysis_csv_1.R
@@ -2,18 +2,26 @@ library(gmp)
 library(date)
 options(scipen = 100)
 options(digits = 10)
-filePrefix = "E:\\My Documents"
+# filePrefix = "E:\\My Documents"
+filePrefix = "d:\\Documents\\工作文档"
 filePath = "\\DRC\\documents\\发币相关"
-fileName = "\\发币2018-4-29-2.csv"
+fileName = "\\发币2019-3-4.csv"
 fileFullPath = paste(filePrefix, filePath, fileName, sep = "")
 input.df = read.csv(fileFullPath, header = FALSE, colClasses = 'character')
 input.df[,2] = paste(input.df[,2], "e18", sep = "")
 input.df
 
+addresses = c(input.df[,1])
+addresses
+
+invalid = addresses[nchar(addresses, type="width") != 42]
+invalid
+which (substring(addresses, 1, 2) != "0x") 
+
 len = length(input.df[,2])
 len
 
-interval = 150
+interval = 200
 if (len < interval) {
   m = c(1)
   n = c(len)
@@ -28,7 +36,7 @@ end = ceiling(len / interval)
 end
 datetime = format(Sys.Date(), "%Y%m%d")
 datetime
-serialNo = 2
+serialNo = 1
 outputFileName = paste("\\addresses", datetime, "-", serialNo, "-", sep = "")
 for (i in 1:end) {
     outputFile = paste(filePrefix, filePath, outputFileName, as.character(i), ".txt", sep = "")
