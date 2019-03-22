9ed5bb13073cffcfb6ac80c90546820d144b1926
Zhi Zhang <zhangzhi@rcitech.cn>
2019-03-23 01:16:13 +0800

    remove no used migration js files

--- migrations/2_deploy_ReleaseToken.js
+++
@@ -1,8 +0,0 @@
-var ReleaseToken = artifacts.require("ReleaseToken");
-
-module.exports = function(deployer) {  
-  deployer.deploy(ReleaseToken, {gas: '5775218', gasPrice: '4000000000'})
-  .then(function(instance) {
-    console.log(instance);
-  });
-};
--- migrations/4_deploy_MintDRCT.js
+++
@@ -1,8 +0,0 @@
-var MintDRCT = artifacts.require("MintDRCT");
-
-module.exports = function(deployer) {  
-  deployer.deploy(MintDRCT, {gas: '5775218', gasPrice: '20000000000'})
-  .then(function(instance) {
-    console.log(instance);
-  });
-};
